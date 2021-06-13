f0f3ded8209c430dcc8af80afae904e4c80ecf77
Otman Bouchari <obouchari@gmail.com>
2021-06-13 13:32:25 +0100

    Refactor var to const Delete useless files from the project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// eslint-disable-next-line no-unused-vars
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// eslint-disable-next-line no-unused-vars
+router.get("/:id", (req, res, next) => {
+  res.json({ success: true });
 });
 
 module.exports = router;
